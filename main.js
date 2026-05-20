7440195f3bbbdb6b3334e4ebb01936bd8201c119
Miguel-Senna <miguel.senna@icloud.com>
2026-05-20 17:18:45 -0300

    added function plotter, still requires input boxes

---
+++ main.js
@@ -0,0 +1,23 @@
+let contentsBounds = document.body.getBoundingClientRect();
+let width = 800;
+let height = 500;
+let ratio = contentsBounds.width / width;
+width *= ratio;
+height *= ratio;
+
+functionPlot({
+  target: "#func",
+  width,
+  height,
+  yAxis: { domain: [-1, 9] },
+  grid: true,
+  data: [
+    {
+      fn: "x^2",
+      derivative: {
+        fn: "2 * x",
+        updateOnMouseMove: true
+      }
+    }
+  ]
+});
