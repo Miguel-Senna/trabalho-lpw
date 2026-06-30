3bf52a1f8d161fd1757f263822491c3eaf948bed
Miguel-Senna <miguel.senna@icloud.com>
2026-06-29 21:30:53 -0300

    agora sim vai

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+let enlarged_div = document.getElementById("enlarged")
+let enlarged_header = document.getElementById("enlarged title")
+let enlarged_paragraph_afim = document.getElementById("enlarged text-afim")
+let enlarged_paragraph_quad = document.getElementById("enlarged text-quadratica")
+let enlarged_paragraph_mod = document.getElementById("enlarged text-modular")
+
+let box1 = document.getElementById("box1")
+let box2 = document.getElementById("box2")
+let box3 = document.getElementById("box3")
 
 function hideCommon() {
     document.getElementById("commonDefTxt").style.display = "none";
@@ -16,9 +25,37 @@ function hideFormal() {
 function hidepopup() {
   document.getElementById("popupOverlay").style.display = "none";
 }
+
 function displayelement() {
-    document.getElementById("enlarged").style.display = "block";
-    document.getElementById("enlarged-overlay").style.display = "block";
+    enlarged_div.style.display = "flex";
+    en.style.display = "block";
+}
+function select_afim() {
+    enlarged_div.style.display = "flex"
+    box1.style.opacity = "0"
+    enlarged_header.textContent = "Função afim"
+    enlarged_paragraph_afim.style.display = "block"
+}
+function select_quad() {
+    enlarged_div.style.display = "flex"
+    box2.style.opacity = "0"
+    enlarged_header.textContent = "Função quadrática"
+    enlarged_paragraph_quad.style.display = "block"
+}
+function select_mod() {
+    enlarged_div.style.display = "flex"
+    box3.style.opacity = "0"
+    enlarged_header.textContent = "Função modular"
+    enlarged_paragraph_mod.style.display = "block"
+}
+function hide_all_select() {
+    box1.style.opacity = "1"
+    box2.style.opacity = "1"
+    box3.style.opacity = "1"
+    enlarged_div.style.display = "none"
+    enlarged_paragraph_mod.style.display = "none"
+    enlarged_paragraph_quad.style.display = "none"
+    enlarged_paragraph_afim.style.display = "none"
 }
 if (navigator.userAgent.toLowerCase().includes("firefox")) {
     document.getElementById("warning").style.display = "none";
